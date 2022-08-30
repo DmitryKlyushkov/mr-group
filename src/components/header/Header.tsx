1fe01f90f8be9aeca1ebffbc35a900d63e17996e
Dmitry Klyushkov <charlesfxa@gmail.com>
2022-08-30 18:11:51 +0300

    favicons, homepage SEO tags

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { FC } from "react";
+import styles from "./Header.module.scss";
+
+const Header: FC = () => {
+  return (
+    <header>
+      <div>
+        <div></div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ru">
+        <Head>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, user-scalable=yes"
+          />
+          <meta name="format-detection" content="telephone=no" />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
